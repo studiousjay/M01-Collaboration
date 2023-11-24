e0073feb869dd5495ab06fcfb86923a7d9807227
studiousjay <149316211+studiousjay@users.noreply.github.com>
2023-11-24 02:37:51 -0500

    Add files via upload
    mod 3 assignment

---
+++ M03 Programming Assignment.py
@@ -0,0 +1,8 @@
+        #M03 Programming Assignment
+
+things=['mozzarella', 'cinderella', 'salmonella']
+things[1]='Cinderella'
+print (things)
+things[0]='MOZZARELLA'
+del things[2]
+print (things)
---
+++ M03 Programming assignment part 2.py
@@ -0,0 +1,13 @@
+#M03 Programming assignment part 2
+
+def good():
+    mylist=['Harry', 'Ron', 'Hermione']
+    print(mylist)
+
+def get_odds():
+    x=range(1,10,2)
+    for i in x:
+        if i==3:
+            print(i)
+
+get_odds()
